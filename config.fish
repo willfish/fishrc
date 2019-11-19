672bd76c822112e454896db32007440f2f0437c4
William Fish <william.michael.fish@gmail.com>
2019-11-19 11:34:15 +0000

    Updates fish config after leaving mydrive

--- config.fish
+++ config.fish
@@ -8,6 +8,7 @@ end
 
 prepend_to_path "$HOME/bin"
 prepend_to_path "$HOME/.local/bin"
+# prepend_to_path "/snap/bin"
 
 set -gx AWS_PROFILE production
 set -gx AWS_REGION "eu-west-1"
@@ -15,5 +16,11 @@ set -gx EDITOR /usr/bin/nvim
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
 set -gx LESS "-R"
 set -gx fish_greeting ""
+
+function harsh -d "Danger find pids relating to program and harsh kill them"
+  for pid in (ps uax | grep $argv[1] | grep -v grep | awk '{print $2}')
+    kill -9 $pid
+  end
+end
 
 source ~/.asdf/asdf.fish
